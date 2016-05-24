63ec39252048612738f8ee8beb518e75e2f3272f
shouston3 <shouston3@me.com>
2016-05-24 16:46:07 +0100

    sending a request to admin site

--- src/routes/refer.js
+++ src/routes/refer.js
@@ -1,18 +1,24 @@
+var http = require('http')
+
 module.exports = {
   method: 'get',
   path: '/refer',
-  handler: (requst, reply) => {
-    reply.view('refer', {
-      about: [
-        {
-          subtitle: 'Title 1',
-          paragraph: 'Lorem Ipsum'
-        },
-        {
-          subtitle: 'Title 2',
-          paragraph: 'Lorem Ipsum sexy Petal'
-        }
-      ]
+  handler: (request, reply) => {
+    var replying = ''
+    const options = {
+      method: 'GET',
+      hostname: 'localhost',
+      port: 4000,
+      path: '/referapi'
+    }
+    const req = http.request(options, res => {
+      res.on('data', chunk => {
+        replying += chunk
+      })
+      res.on('end', () => {
+        reply.view('refer', { refer: JSON.parse(replying) })
+      })
     })
+    req.end()
   }
 }

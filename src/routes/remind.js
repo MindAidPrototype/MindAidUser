fac1a9f0d853ec37920ea80092f4b880ab2444c7
shouston3 <shouston3@me.com>
2016-06-01 16:51:01 +0100

    Modified the remind route to use reds and removed static data and made login button link to the login page

--- src/routes/remind.js
+++ src/routes/remind.js
@@ -1,18 +1,7 @@
-module.exports = {
+module.exports = (client) => ({
   method: 'get',
   path: '/remind',
   handler: (requst, reply) => {
-    reply.view('remind', {
-      about: [
-        {
-          subtitle: 'Title 1',
-          paragraph: 'Lorem Ipsum'
-        },
-        {
-          subtitle: 'Title 2',
-          paragraph: 'Lorem Ipsum sexy Petal'
-        }
-      ]
-    })
+    reply.view('remind')
   }
-}
+})

15a1992fc476aa834ce261c1e5a6fd480d53a5ed
shouston3 <shouston3@me.com>
2016-06-02 10:51:56 +0100

    Gets all the students from the db and renders them to the front for handlebars

--- src/routes/remind.js
+++ src/routes/remind.js
@@ -1,7 +1,16 @@
 module.exports = (client) => ({
   method: 'get',
   path: '/remind',
-  handler: (requst, reply) => {
-    reply.view('remind')
+  handler: (request, reply) => {
+    const user = request.state.cookie
+    if(user) {
+      client.hgetall(user, (err, res) => {
+        reply.view('remind', {
+          remind: JSON.parse(res.s)
+        })
+      })
+    } else {
+      reply.redirect('/login')
+    }
   }
 })

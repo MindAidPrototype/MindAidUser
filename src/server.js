37c8f03ab08156bcb51ef2d53bc2e46267970e47
RobStallion <Robertefrancis18@gmail.com>
2016-06-02 11:33:57 +0100

    add redis url for heroic and localhost for local usage

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const Hapi = require('hapi')
 const port = process.env.PORT || 3000
 const Vision = require('vision')
 const Inert = require('inert')
-const client = require('redis').createClient()
+const redisURL = process.env.REDIS_URL || '//localhost:6379'
+const client = require('redis').createClient(redisURL)
 require('env2')('./config.env')
 
 const views = require('./routes/views.js')

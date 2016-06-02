99d17add3d891eba3c7ecd21dd05fdd14be8c1e9
shouston3 <shouston3@me.com>
2016-06-02 14:57:03 +0100

    Fixing error for inserting a new student with no current student info

--- src/routes/newStudentInfo.js
+++ src/routes/newStudentInfo.js
@@ -6,12 +6,12 @@ module.exports = (client) => ({
       if(Object.keys(res).indexOf('s') > -1) {
         client.hget(request.state.cookie, 's', (err2, res2) => {
           client.hmset(request.state.cookie, 's', JSON.stringify(JSON.parse(res2).concat([JSON.parse(request.payload)])), () => {
-            reply('inserted new student')
+            reply(1)
           })
         })  
       } else {
         client.hmset(request.state.cookie, 's', JSON.stringify([JSON.parse(request.payload)]), () => {
-          reply('inserted first student')
+          reply(1)
         })
       }
     })
--- src/routes/remind.js
+++ src/routes/remind.js
@@ -5,9 +5,13 @@ module.exports = (client) => ({
     const user = request.state.cookie
     if(user) {
       client.hgetall(user, (err, res) => {
-        reply.view('remind', {
-          remind: JSON.parse(res.s)
-        })
+        if(Object.keys(res).indexOf('s') > -1) { 
+          reply.view('remind', {
+            remind: JSON.parse(res.s)
+          })
+        } else {
+          reply.view('remind')
+        }
       })
     } else {
       reply.redirect('/login')
